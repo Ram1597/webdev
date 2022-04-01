43147342fe5ca2ad9faf68e29248d70c02aa23e6
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-04-01 10:27:22 +0530

    day17.4

--- day17.js
+++ day17.js
@@ -1,10 +1,14 @@
-var ch=document.getElementById('head')
+var body=document.querySelector('body')
 // var fun=document.getElementsByClassName('tail')
 // var tag=document.getElementsByTagName('p')
 
 // console.log(ch)
 // console.log(fun[0])
 
-head.innerHTML="lets try js"
-head.style="color:red"
-console.log(head.classList)
+// head.innerHTML="lets try js"
+// head.style="color:red"
+// console.log(head.classList)
+
+
+
+body.classList.add('dark')
