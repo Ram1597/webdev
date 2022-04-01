0816f991ec666baa189824d89df087c69218ea1a
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-04-01 10:00:27 +0530

    day17.2

--- day17.js
+++ day17.js
@@ -1,7 +1,7 @@
 var ch=document.getElementById('head')
 var fun=document.getElementsByClassName('tail')
-
+var tag=document.getElementsByTagName('p')
 
 console.log(ch)
 console.log(fun[0])
-console.log(fun[1])
+console.log(tag[0])
