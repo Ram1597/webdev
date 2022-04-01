b8bc9bcd64bb1a24f6a15149ec2fc4c856fccdb2
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-04-01 11:33:27 +0530

    day17.5

--- day17.js
+++ day17.js
@@ -1,4 +1,4 @@
-var body=document.querySelector('body')
+//var body=document.querySelector('body')
 // var fun=document.getElementsByClassName('tail')
 // var tag=document.getElementsByTagName('p')
 
@@ -9,6 +9,16 @@ var body=document.querySelector('body')
 // head.style="color:red"
 // console.log(head.classList)
 
+var list=document.querySelector('ul')
+var arr=['Two','Three','Four']
+for(var i=0;i<arr.length;i++)
+{
+    var element=document.createElement('li')
+    var textbox=document.createTextNode(arr[i])
+    element.appendChild(textbox)
+    list.appendChild(element)
+
+}
 
 
-body.classList.add('dark')
+
