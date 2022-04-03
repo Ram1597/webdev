6c23dc2fa11735be317b01428a5c40e786b3b4e8
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-04-03 07:48:58 +0530

    day18.6

--- day18.js
+++ day18.js
@@ -3,15 +3,23 @@ var button= document.querySelector('button')
 var input= document.querySelector('input')
 var body=document.querySelector('body')
 
+var chore=[]
 
 const callbackfunc = (event) => {
-        body.classList.add('dark')
-//    // const inputvalue=input.value
-//     const element=document.createElement('li')
-//     const textbox=document.createTextNode(input.value)
-//     element.appendChild(textbox)
-//     list.appendChild(element)
+    
+    const inputvalue=input.value
+    if(chore.includes(inputvalue)) 
+    console.log("Already Present")
+    else
+    {
+        chore.push(inputvalue)
+        const element=document.createElement('li')
+    const textbox=document.createTextNode(input.value)
+    element.appendChild(textbox)
+    list.appendChild(element)
+    }
+    
 
-// }
+}
 
 button.addEventListener('click', callbackfunc)
