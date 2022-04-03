315f0777c017c15fdafb7d880726cd43e48bb8f9
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-04-03 07:05:34 +0530

    day19.1

--- day19.js
+++ day19.js
@@ -6,15 +6,24 @@ var mul=document.querySelector("#mul")
 var res=document.querySelector(".resul")
 
 
-const addfun=() => { 
-   const result=parseInt(input1.value)+parseInt(input2.value)
-   res.innerHTML+=result
+// const addfun=() => { 
+//    const result=parseInt(input1.value)+parseInt(input2.value)
+//    res.innerHTML+=result
+
+// }
+// const mulfun=() => {
+//     const result=parseInt(input1.value)*parseInt(input2.value)
+//     res.innerHTML+=result
+// }
+
+const cal=(event,operation) => {
+    console.log(event.target)
+    if(operation=='add')
+    console.log('add')
+    else if(operation=="mul") console.log('mul') 
+
 
 }
-const mulfun=() => {
-    const result=parseInt(input1.value)*parseInt(input2.value)
-    res.innerHTML+=result
-}
 
-add.addEventListener('click',addfun)
-mul.addEventListener('click',mulfun)
+add.addEventListener('click',(event)=>{cal(event,'add')})
+mul.addEventListener('click',(event)=>{cal(event,'mul')})
