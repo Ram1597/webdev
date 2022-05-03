ea6ce58c85fa6879c03101bd8a5875fb4d3ffbb7
Ram1597 <m.sriramachandrachowdari@gmail.com>
2022-05-03 19:04:19 +0530

    day20.1

---
+++ day20.js
@@ -0,0 +1,7 @@
+let ch={
+        name:"classic",
+        manufacturer:"Mercedes"
+}
+
+console.log(ch.name)
+console.log(ch.manufacturer)
---
+++ pract.js
@@ -0,0 +1,8 @@
+var button=document.querySelector('button')
+var body=document.querySelector('body')
+
+const callback=(event)=>{
+    body.classList.toggle('dark')
+}
+
+button.addEventListener('click',callback)
